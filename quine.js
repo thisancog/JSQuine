3588fa60faccef181ec8208dd6ddf31125c59496
Matthias Planitzer <thisancog@gmail.com>
2017-03-04 12:42:07 +0100

    Update quine.js

--- quine.js
+++ quine.js
@@ -1,1 +1,1 @@
-a="YT0iISI7YWxlcnQoYXRvYihhKS5yZXBsYWNlKFN0cmluZy5mcm9tQ2hhckNvZGUoMzMpLGEpKQ==";alert(atob(a).replace(String.fromCharCode(33),a))
+a="YT0iISI7YWxlcnQoYXRvYihhKS5yZXBsYWNlKCchJyxhKSk=";alert(atob(a).replace('!',a))
